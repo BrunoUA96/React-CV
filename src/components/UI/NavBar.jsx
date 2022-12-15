5a7afe711963eb37a7b8ef87c1825712e18f5af8
BrunoUA96 <ostapklysa@gmail.com>
2022-12-15 23:27:53 +0000

    dark Theme function & pass theme to sassionStorage

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -1,39 +1,68 @@
 import React from 'react';
-import {Link, NavLink} from "react-router-dom";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 function NavBar() {
-    return (
-        <nav className="nav">
-            <div className="container">
-                <div className="nav-row">
-                    <Link to="/" className="logo"><strong>Portfo</strong>lio</Link>
+   const pageView = sessionStorage.getItem('pageView');
+   const [darkTheme, setDarkTheme] = useState(pageView === 'true' ? true : false);
 
-                    <button className="dark-mode-btn">
-                        <img src={`${process.env.PUBLIC_URL}/img/icons/sun.svg`} alt="Light mode" className="dark-mode-btn__icon"/>
-                        <img src={`${process.env.PUBLIC_URL}/img/icons/moon.svg`} alt="Dark mode" className="dark-mode-btn__icon"/>
-                    </button>
+   // Set Dark Theme
+   useEffect(() => {
+      const body = document.querySelector('body');
+      darkTheme ? body.classList.add('dark') : body.classList.remove('dark');
 
-                    <ul className="nav-list">
-                        <li className="nav-list__item">
-                            <NavLink to='/' className="nav-list__link">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-list__item">
-                            <NavLink to='/projects' className="nav-list__link">
-                                Projects
-                            </NavLink>
-                        </li>
-                        <li className="nav-list__item">
-                            <NavLink to='/contacts' className="nav-list__link">
-                                Contacts
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
+      // Pass changes to sassionStorage
+      sessionStorage.setItem('pageView', darkTheme);
+
+      // Change State
+      setDarkTheme(darkTheme);
+   }, [darkTheme]);
+
+   return (
+      <nav className="nav">
+         <div className="container">
+            <div className="nav-row">
+               <Link to="/" className="logo">
+                  <strong>Portfo</strong>lio
+               </Link>
+
+               <button
+                  className={`dark-mode-btn ${darkTheme ? 'dark-mode-btn--active' : ''}`}
+                  onClick={() => setDarkTheme(!darkTheme)}>
+                  <img
+                     src={`${process.env.PUBLIC_URL}/img/icons/sun.svg`}
+                     alt="Light mode"
+                     className="dark-mode-btn__icon"
+                  />
+                  <img
+                     src={`${process.env.PUBLIC_URL}/img/icons/moon.svg`}
+                     alt="Dark mode"
+                     className="dark-mode-btn__icon"
+                  />
+               </button>
+
+               <ul className="nav-list">
+                  <li className="nav-list__item">
+                     <NavLink to="/" className="nav-list__link">
+                        Home
+                     </NavLink>
+                  </li>
+                  <li className="nav-list__item">
+                     <NavLink to="/projects" className="nav-list__link">
+                        Projects
+                     </NavLink>
+                  </li>
+                  <li className="nav-list__item">
+                     <NavLink to="/contacts" className="nav-list__link">
+                        Contacts
+                     </NavLink>
+                  </li>
+               </ul>
             </div>
-        </nav>
-    );
+         </div>
+      </nav>
+   );
 }
 
 export default NavBar;
