69657c8305f3fa4ecaf4909a5c7d6042dff4d7ab
BrunoUA96 <ostapklysa@gmail.com>
2022-12-15 23:51:47 +0000

    Add CV

--- src/components/UI/Banner.jsx
+++ src/components/UI/Banner.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 
 function Banner() {
-    return (
-        <header className="header" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/header-bg.png'})`}}>
-            <div className="header__wrapper">
-                <h1 className="header__title">
-                    <strong>
-                        Hi, my name is
-                        <em> Ostap</em>
-                    </strong>
-                    <br/>
-                    a front-end developer
-                </h1>
-                <div className="header__text">
-                    <p>Passionate developer offering 5+ years of experience, strong sense of web design and commitment
-                        to the user
-                        experience, including the accessibility.
-                        Working on wide range of project types, from landing pages to large platforms.
-                        Fast adaptation to new Technologies, Frameworks and Program Languages.
-                        Comfortable with working with code written by others, and development from scratch.
-                        Excellent communication skills and great collaborator</p>
-                </div>
-                <a href="#!" className="btn">Download CV</a>
+   return (
+      <header
+         className="header"
+         style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/img/header-bg.png'})` }}>
+         <div className="header__wrapper">
+            <h1 className="header__title">
+               <strong>
+                  Hi, my name is
+                  <em> Ostap</em>
+               </strong>
+               <br />a front-end developer
+            </h1>
+            <div className="header__text">
+               <p>
+                  Passionate developer offering 5+ years of experience, strong sense of web design
+                  and commitment to the user experience, including the accessibility. Working on
+                  wide range of project types, from landing pages to large platforms. Fast
+                  adaptation to new Technologies, Frameworks and Program Languages. Comfortable with
+                  working with code written by others, and development from scratch. Excellent
+                  communication skills and great collaborator
+               </p>
             </div>
-        </header>
-    );
+            <a href={`${process.env.PUBLIC_URL + '/docs/Resume-Ostap-Klysa.pdf'}`} className="btn">
+               Download CV
+            </a>
+         </div>
+      </header>
+   );
 }
 
 export default Banner;
