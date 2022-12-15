5603600cd4d2682fe82093bcb31251c3944132af
BrunoUA96 <ostapklysa@gmail.com>
2022-12-15 23:36:49 +0000

    router redirect to home  page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import './css/main.css';
-import {Routes, Route} from "react-router-dom";
-import NavBar from "./components/UI/NavBar";
-import Footer from "./components/UI/Footer";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import PageNotFound from "./pages/PageNotFound";
-import Project from "./pages/Project";
-import Contacts from "./pages/Contacts";
+import { Routes, Navigate, Route } from 'react-router-dom';
+import NavBar from './components/UI/NavBar';
+import Footer from './components/UI/Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Project from './pages/Project';
+import Contacts from './pages/Contacts';
 
 function App() {
-    return (
-        <>
-            <NavBar/>
+   return (
+      <>
+         <NavBar />
 
-            <Routes>
-                <Route exact path='/' element={<Home/>}></Route>
-                <Route path='/projects' element={<Projects/>}></Route>
-                <Route path='/project/:id' element={<Project/>}></Route>
-                <Route path='/contacts' element={<Contacts/>}></Route>
+         <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/project/:id" element={<Project />}></Route>
+            <Route path="/contacts" element={<Contacts />}></Route>
 
-                <Route path="*" element={<PageNotFound/>}/>
-            </Routes>
+            {/* Redirect to Home page */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+         </Routes>
 
-            <Footer/>
-        </>
-    );
+         <Footer />
+      </>
+   );
 }
 
 export default App;
